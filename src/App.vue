541016211e368e3d55ae0d32da2346b9516e6b6e
Jan Wolfgang Hiermayer <if18b041@technikum-wien.at>
2021-01-14 13:34:47 +0100

    edited

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <app-navigation></app-navigation>
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNavigation from './components/AppNavigation.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppNavigation
   }
+    
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
