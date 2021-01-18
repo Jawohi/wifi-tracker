4cc53619f5c88e88079423c9a5ec205ec294b209
Jan Wolfgang Hiermayer <if18b041@technikum-wien.at>
2021-01-18 21:32:39 +0100

    fixed errors with response and declarations

--- tracker/js/main.js
+++ tracker/js/main.js
@@ -1,4 +1,5 @@
 // initialize config variables here
+import 'regenerator-runtime/runtime'
 
 class TrackingData
 {
@@ -47,6 +48,7 @@ let canvas, ctx
 document.addEventListener('DOMContentLoaded', init)
 
 
+
 async function init() {
 
   var trackingData = await getTrackingData()
@@ -75,7 +77,7 @@ async function init() {
 
   setInterval(async () => {
 
-    var trackingData = await getTrackingData()
+    trackingData = await getTrackingData()
 
     for (let i = 0; i < trackingData.clients.length; i++) { //create rect for each tracked device
       createRect(trackingData.clients[i].x * factorX, trackingData.clients[i].y * factorY, 'orange')
@@ -91,8 +93,8 @@ async function getTrackingData() {
 
 
   var res = await fetch("http://localhost/device-tracker-backend-master/track.php");
-  var data = await res.json();
-  var trackingdata = JSON.parse(data);
+  var trackingdata = await res.json();
+ // var trackingdata = JSON.parse(data);  // nicht benötigt glaube ich
 
   /*
   var json = '{"clients":[{"mac":"e0:cb:ee:46:58:fa","x":2.000251321150406,"y":2.298495242436292,"acc":0.5},{"mac":"e0:cb:ee:46:58:fa","x":3.800251321150406,"y":2.798495242436292,"acc":0.5}],"tracker":[{"name":"Tracker: 1","x":2.4,"y":0},{"name":"Tracker: 2","x":0,"y":3.5},{"name":"Tracker: 3","x":4,"y":3.5},{"name":"Tracker: 4","x":4,"y":2.4},{"name":"Tracker: 5","x":4,"y":0}],"room":{"x":4.5,"y":4}}'
